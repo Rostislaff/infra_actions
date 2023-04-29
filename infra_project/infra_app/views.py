9cf7c587af113a94179ca7c0c40f20bc59f2f1c2
Rostislaff <rossanna8368@gmail.com>
2023-04-29 14:53:25 +0300

    deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Даже несмотря на то, что вы не сказали про элоухост!')
 
 
 def second_page(request):
