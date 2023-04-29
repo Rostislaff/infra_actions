bfec171f0b443f0a4466875c06ba8c165204e7c4
Rostislaff <rossanna8368@gmail.com>
2023-04-29 14:59:47 +0300

    deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось c на allowhost!')
+    return HttpResponse('У меня получилось даже с allowhost!')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница!')
+    return HttpResponse('שבת שלום .А это вторая страница!')
